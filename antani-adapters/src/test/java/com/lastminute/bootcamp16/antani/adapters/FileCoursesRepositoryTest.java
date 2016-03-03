47f3c9408921982e2e73581e8b05a166b5a9c87b
cccanargiu <cristina.canargiu@rumbo.com>
2016-03-03 16:54:10 +0100

    reader from file

--- antani-adapters/src/test/java/com/lastminute/bootcamp16/antani/adapters/FileCoursesRepositoryTest.java
+++ antani-adapters/src/test/java/com/lastminute/bootcamp16/antani/adapters/FileCoursesRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.lastminute.bootcamp16.antani.adapters;
 
+import com.lastminute.bootcamp16.antani.domain.Course;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -9,7 +12,8 @@ public class FileCoursesRepositoryTest
 {
 
   @Test
-  public void noCourses(){
+  public void noCourses()
+  {
 
     String file = "db/empty.csv";
 
@@ -18,12 +22,34 @@ public class FileCoursesRepositoryTest
   }
 
   @Test
-    public void oneCourses(){
+  public void oneCourses()
+  {
 
-      String file = "db/one.csv";
+    String file = "db/one.csv";
 
-      FileCoursesRepository repository = new FileCoursesRepository(file);
+    FileCoursesRepository repository = new FileCoursesRepository(file);
 
-      assertThat(repository.retrieveAll().size(), is(1));
-    }
+    final List<Course> courses = repository.retrieveAll();
+    assertThat(courses.size(), is(1));
+    final Course actual = courses.get(0);
+    final Course course = new Course("01", "Lingua Kinglon per principianti", 90, "2016-03-02T18:36:14+00:00");
+    assertThat(actual, is(course));
+
+  }
+
+  @Test
+  public void twoCourses()
+  {
+
+    String file = "db/two.csv";
+
+    FileCoursesRepository repository = new FileCoursesRepository(file);
+
+    final List<Course> courses = repository.retrieveAll();
+
+    assertThat(courses.size(), is(2));
+    assertThat(courses.get(0), is(new Course("01", "Lingua Kinglon per principianti", 90, "2016-03-02T18:36:14")));
+    assertThat(courses.get(1), is(new Course("02", "Clean Code", 200, "2016-04-02T18:36:14")));
+
+  }
 }
--- antani-domain/src/main/java/com/lastminute/bootcamp16/antani/domain/Course.java
+++ antani-domain/src/main/java/com/lastminute/bootcamp16/antani/domain/Course.java
@@ -1,5 +1,9 @@
 package com.lastminute.bootcamp16.antani.domain;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public class Course
 {
   private final String code;
@@ -9,13 +13,28 @@ public class Course
 
   public Course(String code, String title, int price, String dateTime)
   {
-
     this.code = code;
     this.title = title;
     this.price = price;
     this.dateTime = dateTime;
   }
 
+  @Override
+  public boolean equals(Object o)
+  {
 
+    return EqualsBuilder.reflectionEquals(this,o);
+  }
 
+  @Override
+  public int hashCode()
+  {
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
+
+  @Override
+  public String toString()
+  {
+    return ToStringBuilder.reflectionToString(this);
+  }
 }
