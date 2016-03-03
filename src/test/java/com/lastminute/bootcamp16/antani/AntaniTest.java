1fe5e0ad10259a6f80b283165ec8c34165c55b3d
Antonino Cortese <antonino.cortese@rumbo.com>
2016-03-03 10:51:40 +0100

    First failing test

---
+++ src/test/java/com/lastminute/bootcamp16/antani/AntaniTest.java
@@ -0,0 +1,15 @@
+package com.lastminute.bootcamp16.antani;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AntaniTest
+{
+  @Test
+  public void firstTest() throws Exception
+  {
+    assertThat(false, is(true));
+  }
+}
