d48b4af35de05251d46d9f3d21e1c01da6974046
cccanargiu <cristina.canargiu@rumbo.com>
2016-03-03 14:54:27 +0100

    introduce repository draft

--- antani-domain/src/main/java/com/lastminute/bootcamp16/antani/domain/RetrieveCourses.java
+++ antani-domain/src/main/java/com/lastminute/bootcamp16/antani/domain/RetrieveCourses.java
@@ -1,12 +1,31 @@
 package com.lastminute.bootcamp16.antani.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RetrieveCourses
 {
+  private final List<Course> courses;
+
+  public RetrieveCourses(List<Course> courses)
+  {
+    this.courses = courses;
+  }
+
   public List<Course> all()
   {
-    return new ArrayList<Course>();
+    return new FileCoursesRepository(courses).invoke();
+  }
+
+  private class FileCoursesRepository
+  {
+    private final List<Course> courses;
+
+    public FileCoursesRepository(List<Course> courses)
+    {
+
+      this.courses = courses;
+    }
+
+    public List<Course> invoke() {return this.courses;}
   }
 }
--- antani-domain/src/test/java/com/lastminute/bootcamp16/antani/domain/RetrieveCoursesTest.java
+++ antani-domain/src/test/java/com/lastminute/bootcamp16/antani/domain/RetrieveCoursesTest.java
@@ -2,10 +2,11 @@ package com.lastminute.bootcamp16.antani.domain;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class RetrieveCoursesTest
 {
@@ -14,9 +15,21 @@ public class RetrieveCoursesTest
   public void noCourses() throws Exception
   {
 
-    RetrieveCourses retrieveCourse = new RetrieveCourses();
+    RetrieveCourses retrieveCourse = new RetrieveCourses(new ArrayList<Course>());
     List<Course> courses = retrieveCourse.all();
     assertThat(courses.size(), is(0));
+
+  }
+
+  @Test
+  public void oneCourse() throws Exception
+  {
+    Course course = new Course();
+    final ArrayList<Course> courses = new ArrayList<Course>();
+    courses.add(course);
+    RetrieveCourses retrieveCourse = new RetrieveCourses(courses);
+
+    assertThat(retrieveCourse.all().size(), is(1));
 
   }
 }
