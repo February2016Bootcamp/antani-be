2b65396d30a785740c51523243cdfc8e93d70b60
sintroini <stefano.introini@rumbo.com>
2016-03-04 16:19:54 +0100

    Added controller for rest service /participants

--- antani-main/src/main/java/com/lastminute/bootcamp16/antani/controller/CoursesController.java
+++ antani-main/src/main/java/com/lastminute/bootcamp16/antani/controller/CoursesController.java
@@ -1,35 +1,48 @@
 package com.lastminute.bootcamp16.antani.controller;
 
 import com.lastminute.bootcamp16.antani.adapters.FileCoursesRepository;
+import com.lastminute.bootcamp16.antani.adapters.InMemoryParticipantsRepository;
 import com.lastminute.bootcamp16.antani.domain.Course;
+import com.lastminute.bootcamp16.antani.domain.RegisterParticipant;
 import com.lastminute.bootcamp16.antani.domain.RetrieveCourses;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.PathParam;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
+@CrossOrigin
 public class CoursesController
 {
   private RetrieveCourses retrieveCourses;
+  private final RegisterParticipant registerParticipant;
 
-  public CoursesController(RetrieveCourses retrieveCourses)
+  public CoursesController(RetrieveCourses retrieveCourses, RegisterParticipant registerParticipant)
   {
     this.retrieveCourses = retrieveCourses;
+    this.registerParticipant = registerParticipant;
   }
   public CoursesController()
    {
-     this.retrieveCourses = new RetrieveCourses(new FileCoursesRepository("db.csv"));
+     this(new RetrieveCourses(new FileCoursesRepository("db.csv")),
+          new RegisterParticipant(new InMemoryParticipantsRepository()));
    }
 
-  @CrossOrigin
   @RequestMapping(value = "/courses", method = GET)
   public List<Course> retrieveAllCourses()
   {
     List<Course> all = retrieveCourses.all();
     return all;
+  }
+
+  @RequestMapping(value = "/courses/{code}/partecipants", method = POST)
+  public void register(@PathParam("code") String code)
+  {
+    registerParticipant.register(code);
   }
 }
--- antani-main/src/test/java/com/lastminute/bootcamp16/antani/controller/CoursesControllerTest.java
+++ antani-main/src/test/java/com/lastminute/bootcamp16/antani/controller/CoursesControllerTest.java
@@ -1,6 +1,7 @@
 package com.lastminute.bootcamp16.antani.controller;
 
 import com.lastminute.bootcamp16.antani.domain.Course;
+import com.lastminute.bootcamp16.antani.domain.RegisterParticipant;
 import com.lastminute.bootcamp16.antani.domain.RetrieveCourses;
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
@@ -12,7 +13,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class CoursesControllerTest
 {
@@ -20,11 +21,12 @@ public class CoursesControllerTest
   @Rule
   public JUnitRuleMockery context = new JUnitRuleMockery();
 
-  private RetrieveCourses retrieveCourses ;
+  private RetrieveCourses retrieveCourses;
 
 
   @Before
-  public void setUp(){
+  public void setUp()
+  {
     context.setImposteriser(ClassImposteriser.INSTANCE);
     retrieveCourses = context.mock(RetrieveCourses.class);
   }
@@ -32,13 +34,29 @@ public class CoursesControllerTest
   @Test
   public void retrieveAllCourses() throws Exception
   {
-    context.checking(new Expectations(){{
+    context.checking(new Expectations()
+    {{
 
-      allowing(retrieveCourses).all();
-      will(returnValue(Arrays.asList(new Course(null,null,1,null),new Course(null,null,1,null))));
-    }});
+        allowing(retrieveCourses).all();
+        will(returnValue(Arrays.asList(new Course(null, null, 1, null), new Course(null, null, 1, null))));
+      }});
 
-    CoursesController controller = new CoursesController(retrieveCourses);
+    CoursesController controller = new CoursesController(retrieveCourses,null);
     assertThat(controller.retrieveAllCourses().size(), is(2));
+  }
+
+
+  @Test
+  public void registerParticipant()
+  {
+    RegisterParticipant registerParticipant = context.mock(RegisterParticipant.class);
+    context.checking(new Expectations()
+    {{
+
+        oneOf(registerParticipant).register("01");
+      }});
+
+    CoursesController controller = new CoursesController(null, registerParticipant);
+    controller.register("01");
   }
 }
