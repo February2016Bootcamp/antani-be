d70a75ab4c1644e4c5335a48ff01b055eb1235f2
fdeluca <francesco.deluca@external-rumbo.com>
2016-03-04 09:38:45 +0100

    Added Controller Test

--- antani-main/src/main/java/com/lastminute/bootcamp16/antani/controller/CoursesController.java
+++ antani-main/src/main/java/com/lastminute/bootcamp16/antani/controller/CoursesController.java
@@ -1,19 +1,24 @@
 package com.lastminute.bootcamp16.antani.controller;
 
 import com.lastminute.bootcamp16.antani.domain.Course;
+import com.lastminute.bootcamp16.antani.domain.RetrieveCourses;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CoursesController
 {
+  private RetrieveCourses retrieveCourses;
+
+  public CoursesController(RetrieveCourses retrieveCourses)
+  {
+    this.retrieveCourses = retrieveCourses;
+  }
+
   public List<Course> retrieveAllCourses()
   {
-    final ArrayList<Course> courses = new ArrayList<>();
-    courses.add(new Course(null, null, 0, null));
-    courses.add(new Course(null, null, 0, null));
-    return courses;
+    List<Course> all = retrieveCourses.all();
+    return all;
   }
 }
