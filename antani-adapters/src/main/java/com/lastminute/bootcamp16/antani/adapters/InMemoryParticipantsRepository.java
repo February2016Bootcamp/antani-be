94247e35ee113baa5b97de00e2325ca5d9aa55a1
Alberto Minetti <alberto.minetti@rumbo.com>
2016-03-04 16:03:58 +0100

    implemented InMemoryParticipantsRepository logic

--- antani-adapters/src/main/java/com/lastminute/bootcamp16/antani/adapters/InMemoryParticipantsRepository.java
+++ antani-adapters/src/main/java/com/lastminute/bootcamp16/antani/adapters/InMemoryParticipantsRepository.java
@@ -2,17 +2,26 @@ package com.lastminute.bootcamp16.antani.adapters;
 
 import com.lastminute.bootcamp16.antani.domain.ports.ParticipantsRepository;
 
-public class InMemoryParticipantsRepository implements ParticipantsRepository
-{
-  @Override
-  public void register(String courseId)
-  {
+import java.util.HashMap;
+import java.util.Map;
 
+public class InMemoryParticipantsRepository implements ParticipantsRepository {
+  private Map<String, Integer> participants;
+
+  public InMemoryParticipantsRepository() {
+    participants = new HashMap<>();
   }
 
   @Override
-  public int countParticipants(String courseId)
-  {
-    return 0;
+  public void register(String courseId) {
+    Integer count = participants.get(courseId);
+    if (count == null) count = 0;
+    participants.put(courseId, ++count);
+  }
+
+  @Override
+  public int countParticipants(String courseId) {
+    Integer count = participants.get(courseId);
+    return count == null ? 0 : count;
   }
 }
--- antani-adapters/src/test/java/com/lastminute/bootcamp16/antani/adapters/InMemoryParticipantsRepositoryTest.java
+++ antani-adapters/src/test/java/com/lastminute/bootcamp16/antani/adapters/InMemoryParticipantsRepositoryTest.java
@@ -6,14 +6,29 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class InMemoryParticipantsRepositoryTest
-{
+public class InMemoryParticipantsRepositoryTest {
 
   @Test
-  public void noParticipants()
-  {
+  public void noParticipants() {
     ParticipantsRepository repository = new InMemoryParticipantsRepository();
     assertThat(repository.countParticipants("01"), is(0));
+  }
+
+  @Test
+  public void oneParticipant() {
+    ParticipantsRepository repository = new InMemoryParticipantsRepository();
+    repository.register("01");
+    assertThat(repository.countParticipants("01"), is(1));
+  }
+
+  @Test
+  public void differentCoursesParticipants() {
+    ParticipantsRepository repository = new InMemoryParticipantsRepository();
+    repository.register("01");
+    repository.register("01");
+    repository.register("02");
+    assertThat(repository.countParticipants("01"), is(2));
+    assertThat(repository.countParticipants("02"), is(1));
   }
 
 }
