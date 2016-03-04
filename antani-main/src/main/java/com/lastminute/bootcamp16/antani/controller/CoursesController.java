8ba76998fa6ae44a9ae556eeb26d90b4454f2326
Alberto Minetti <alberto.minetti@rumbo.com>
2016-03-04 12:52:49 +0100

    cors enabled

--- antani-main/src/main/java/com/lastminute/bootcamp16/antani/controller/CoursesController.java
+++ antani-main/src/main/java/com/lastminute/bootcamp16/antani/controller/CoursesController.java
@@ -3,6 +3,7 @@ package com.lastminute.bootcamp16.antani.controller;
 import com.lastminute.bootcamp16.antani.adapters.FileCoursesRepository;
 import com.lastminute.bootcamp16.antani.domain.Course;
 import com.lastminute.bootcamp16.antani.domain.RetrieveCourses;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +25,8 @@ public class CoursesController
      this.retrieveCourses = new RetrieveCourses(new FileCoursesRepository("db.csv"));
    }
 
-
+  @CrossOrigin
   @RequestMapping(value = "/courses", method = GET)
-
   public List<Course> retrieveAllCourses()
   {
     List<Course> all = retrieveCourses.all();
